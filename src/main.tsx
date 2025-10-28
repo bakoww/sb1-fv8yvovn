6c9bce157f69af178edcbbf436fb8f3ee0f73d80
mathisdk <mathisdk@MacBook-Air-de-mathisdk.local>
2025-10-28 11:22:35 +0100

    Ajout favicon et injection dans main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// inject favicon when no index.html is edited
+const link = document.createElement('link')
+link.rel = 'icon'
+link.type = 'image/svg+xml'
+link.href = '/favicon.svg'
+document.head.appendChild(link)
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(<App />)
